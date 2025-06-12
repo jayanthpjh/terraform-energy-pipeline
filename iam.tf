9381e427c2fdd40076c20b926eaae0c5ec2b141b
jayanthpjh <58696269+jayanthpjh@users.noreply.github.com>
2025-06-12 01:37:24 -0400

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -15,3 +15,22 @@ resource "aws_iam_policy_attachment" "lambda_access" {
   roles      = [aws_iam_role.lambda_exec.name]
   policy_arn = "arn:aws:iam::aws:policy/AWSLambdaExecute"
 }
+
+resource "aws_iam_role_policy" "lambda_dynamodb_access" {
+  name = "lambda-dynamodb-policy-${random_pet.suffix.id}"
+  role = aws_iam_role.lambda_exec.name
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid: "AllowDynamoDBPut",
+        Effect: "Allow",
+        Action: [
+          "dynamodb:PutItem"
+        ],
+        Resource: aws_dynamodb_table.energy_data.arn
+      }
+    ]
+  })
+}
