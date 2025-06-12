f83ec01cf3b77d1b0c4c14ee95c03002bfc11075
jayanthpjh <jayanthpjh@gmail.com>
2025-06-12 01:47:05 -0400

    last commit

--- lambda/data_processor.py
+++ lambda/data_processor.py
@@ -1,7 +1,20 @@
-import boto3, json, os
+import boto3
+import json
+import os
+from decimal import Decimal
 
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.environ['TABLE_NAME'])
+
+def convert_floats(obj):
+    if isinstance(obj, list):
+        return [convert_floats(i) for i in obj]
+    elif isinstance(obj, dict):
+        return {k: convert_floats(v) for k, v in obj.items()}
+    elif isinstance(obj, float):
+        return Decimal(str(obj))
+    else:
+        return obj
 
 def lambda_handler(event, context):
     s3 = boto3.client("s3")
@@ -12,7 +25,13 @@ def lambda_handler(event, context):
         data = json.loads(obj['Body'].read())
 
         for item in data:
-            anomaly = item['energy_generated_kwh'] < 0 or item['energy_consumed_kwh'] < 0
             item['net_energy_kwh'] = item['energy_generated_kwh'] - item['energy_consumed_kwh']
-            item['anomaly'] = anomaly
-            table.put_item(Item=item)
+            item['anomaly'] = (
+                item['energy_generated_kwh'] < 0 or
+                item['energy_consumed_kwh'] < 0
+            )
+
+            # 💥 CONVERT all float types to Decimal to avoid DynamoDB error
+            safe_item = convert_floats(item)
+
+            table.put_item(Item=safe_item)
